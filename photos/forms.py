248ad584674d00510b36a25373e790a149e1f00f
hadi sql <hadi.sqalli@laplateforme.io>
2023-08-11 01:35:50 +0200

    initial commit for comments feature

--- photos/forms.py
+++ photos/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 
+from comments_likes.models import Comment
 
 class PhotoForm(forms.Form):
     pass
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['body']
+        widgets = {
+            'body': forms.Textarea(attrs={'class':'textarea textarea-primary textarea-xs w-full max-w-lg', 'placeholder': 'Send a comment..','rows':4})
+            }
