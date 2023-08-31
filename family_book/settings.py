2a70ed1f39d6a1147a4168e94587144a29cf880a
hadi sql <hadi.sqalli@laplateforme.io>
2023-08-31 19:56:22 +0200

    removed staticfiles_dirs from settings

--- family_book/settings.py
+++ family_book/settings.py
@@ -150,9 +150,9 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
