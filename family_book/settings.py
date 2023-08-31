80dfe6580b572b11bb7b5f2a1865288e48723d28
hadi sql <hadi.sqalli@laplateforme.io>
2023-08-31 19:22:59 +0200

    edited static_root path cor collectstatic to work

--- family_book/settings.py
+++ family_book/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
