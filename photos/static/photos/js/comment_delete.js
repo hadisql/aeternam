9e485cd44e0bdc293beccce0e38a4c59f52a9a59
hadi sql <hadi.sqalli@laplateforme.io>
2023-10-03 11:17:10 +0200

    fixed comment deletion script issue

--- photos/static/photos/js/comment_delete.js
+++ photos/static/photos/js/comment_delete.js
@@ -1,45 +1,32 @@
 // Photo detail page -> delete comment
 
-// Function to handle button click and toggle visibility
-function del_comment(deleteButton, confirmButton) {
-  deleteButton.classList.add("hidden");
-  confirmButton.classList.remove("hidden");
-}
+$(document).ready(function() {
+  // Function to handle button click and toggle visibility
+  function showHideButtons(deleteButton, confirmButton) {
+    deleteButton.classList.add("hidden");
+    confirmButton.classList.remove("hidden");
+  }
 
-// // Attach event listeners to all button pairs
-// const buttonPairs = document.querySelectorAll(".button-pair");
-// buttonPairs.forEach((pair) => {
-//   const deleteButton = pair.querySelector(".delete-button");
-//   const confirmButton = pair.querySelector(".confirm-button");
+  // Attach event listeners to all button pairs
+  const buttonPairs = document.querySelectorAll(".button-pair");
+  buttonPairs.forEach((pair) => {
+    const deleteButton = pair.querySelector(".delete-button");
+    const confirmButton = pair.querySelector(".confirm-button");
 
-//   deleteButton.addEventListener("click", () => del_comment(deleteButton, confirmButton));
-// });
+    // Event listener for delete button
+    deleteButton.addEventListener("click", (event) => {
+      event.stopPropagation(); // Prevent bubbling up to document
+      showHideButtons(deleteButton, confirmButton);
+    });
 
-// Function to show delete button and hide confirm button
-function showDeleteButton(deleteButton, confirmButton) {
-  deleteButton.classList.remove("hidden");
-  confirmButton.classList.add("hidden");
-}
+    // Event listener for clicking anywhere else on the page
+    document.addEventListener("click", () => {
+      showHideButtons(confirmButton, deleteButton);
+    });
 
-// Attach event listeners to all button pairs
-const buttonPairs = document.querySelectorAll(".button-pair");
-buttonPairs.forEach((pair) => {
-  const deleteButton = pair.querySelector(".delete-button");
-  const confirmButton = pair.querySelector(".confirm-button");
-
-  // Event listener for delete button
-  deleteButton.addEventListener("click", (event) => {
-    event.stopPropagation(); // Prevent bubbling up to document
-    del_comment(deleteButton, confirmButton);
-  });
-
-  // Event listener for clicking anywhere else on the page
-  document.addEventListener("click", () => {
-    showDeleteButton(deleteButton, confirmButton);
-  });
-
-  // Prevent clicking on the confirm button from hiding it immediately
-  confirmButton.addEventListener("click", (event) => {
-    event.stopPropagation();
+    // Prevent clicking on the confirm button from hiding it immediately
+    confirmButton.addEventListener("click", (event) => {
+      event.stopPropagation();
+    });
   });
 });
