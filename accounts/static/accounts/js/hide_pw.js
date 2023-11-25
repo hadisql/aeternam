a5a1c413862358ded2ce68590fc935d900a6afc0
hadi sql <hadi.sqalli@laplateforme.io>
2023-11-25 20:14:03 +0100

    fixed login pw show-hide functionality

--- accounts/static/accounts/js/hide_pw.js
+++ accounts/static/accounts/js/hide_pw.js
@@ -1,11 +1,17 @@
 // Login Page -> hide/show password button
 function reveal() {
-  if((document.getElementById('box').classList.contains('show-pw')) && document.getElementById('box').click){
-    document.getElementById("pw").type="text";
-    document.getElementById('box').classList.remove('show-pw')
-  }
-  else if(document.getElementById('box').click) {
-    document.getElementById("pw").type='password';
-    document.getElementById('box').classList.add('show-pw')
+  if(document.getElementById('box').click){
+    if(!document.getElementById('box').classList.contains('show-pw')) {
+      document.getElementById("pw").type="text";
+      document.getElementById('box').classList.add('show-pw');
+      document.getElementById('pwHide').classList.add('hidden');
+      document.getElementById('pwShow').classList.remove('hidden')
+    }
+    else {
+      document.getElementById("pw").type='password';
+      document.getElementById('box').classList.remove('show-pw');
+      document.getElementById('pwHide').classList.remove('hidden');
+      document.getElementById('pwShow').classList.add('hidden')
+    }
   }
 }
