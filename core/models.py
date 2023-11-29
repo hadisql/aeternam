85364cc1b7230a64b92f1171c845038aa8850a3a
hadi sql <hadi.sqalli@laplateforme.io>
2023-11-29 11:08:46 +0100

    feedbacks feature

--- core/models.py
+++ core/models.py
@@ -3,4 +3,7 @@ from accounts.models import CustomUser
 
 class Feedback(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_feedback')
-    feedback = models.TextField()
+    feedback = models.TextField(null=True)
+
+    def __str__(self) -> str:
+        return f"Feedback from {self.user.first_name or self.user.email}: {self.feedback[:15]}.."
