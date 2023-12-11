b33acde2f09a08a39380c56dfed55e08b4e80784
hadi sql <hadi.sqalli@laplateforme.io>
2023-12-11 13:00:55 +0100

    added disconnect confirmation modal

--- accounts/static/accounts/js/confirm_request.js
+++ accounts/static/accounts/js/confirm_request.js
@@ -1,10 +1,25 @@
 // new connection request
 $(document).ready(function() {
 
+// the following function is triggered for both connection request and connection request cancel (undo the request)
 const btnConfirmRequest = document.getElementById('confirm_request');
 const btnToClick = document.getElementById('request_btn');
 
-  btnConfirmRequest.addEventListener('click', ()=> {
-    btnToClick.click();
-  });
+  if (btnConfirmRequest) {
+    btnConfirmRequest.addEventListener('click', ()=> {
+      btnToClick.click();
+    });
+  }
+
+// the following function is triggered for the disconnect button (confirmation that user wants to disconnect their relation with another user)
+const btnConfirmDisconnect = document.getElementById('confirm_disconnect');
+const btnToClickToDisconnect = document.getElementById('disconnect_btn');
+
+  if (btnConfirmDisconnect){
+    btnConfirmDisconnect.addEventListener('click', ()=> {
+      console.log("disconnect confirmation clicked")
+      btnToClickToDisconnect.click();
+    });
+  }
+
 })
