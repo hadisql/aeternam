3a31f87c67e5aacc83fdca13ad4fedc0428dd0d8
hadi sql <hadi.sqalli@laplateforme.io>
2024-03-10 15:50:53 +0100

    added fake user profile pic auto-upload

--- accounts/management/commands/create_users.py
+++ accounts/management/commands/create_users.py
@@ -2,7 +2,8 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from accounts.models import Relation
 from django.contrib.auth.hashers import make_password
-
+from django.core.files import File
+import os
 import logging
 logger = logging.getLogger(__name__)
 
@@ -18,6 +19,7 @@ class Command(BaseCommand):
         parser.add_argument('--relationwith', type=str, help='Create Relation objects with a specified user (provide the user email)')
         parser.add_argument('--change_existing', action='store_true', help='Change password for existing users if they already exist')
         parser.add_argument('--full_name', type=str, help='Sets the name of the user to save as first and last name')
+        parser.add_argument('--inactive', action='store_true', help='Sets the user as inactive')
 
     def handle(self, *args, **options):
         emails = options['emails']
@@ -25,6 +27,7 @@ class Command(BaseCommand):
         create_relation_between = options['relationbetween']
         relation_with_email = options['relationwith']
         full_name = options['full_name']
+        is_inactive = options['inactive']
 
         users = []
 
@@ -33,26 +36,47 @@ class Command(BaseCommand):
             user, created = User.objects.get_or_create(email=email)
             users.append(user)
 
+            if full_name:
+                # Split the full name into first and last names
+                first_name, last_name = full_name.split(' ', 1) if ' ' in full_name else (full_name, '')
+
+                if first_name:
+                    user.first_name = first_name
+                if last_name:
+                    user.last_name = last_name
+
+                # Try to find a profile picture based on the user's name
+                supported_formats = ['.jpg', '.jpeg', '.png']
+                profile_picture = None
+                for ext in supported_formats:
+                    profile_picture_path = os.path.join('mediafiles', 'fake_profile_pictures', f'{first_name.lower()}_{last_name.lower()}{ext}')
+                    if os.path.exists(profile_picture_path):
+                        logger.info(f"profile picture path : {profile_picture_path}")
+                        profile_picture  = profile_picture_path
+                        break
+                if profile_picture:
+                    with open(profile_picture, 'rb') as f:
+                        user.profile_picture.save(os.path.basename(profile_picture), File(f), save=True)
+
+            else:
+                # If full name not provided, use the first part as first name
+                user.first_name = email.split('@')[0]
+
             if created or options['change_existing']:
                 user.password = make_password(password)
 
-                if full_name:
-                    # Split the full name into first and last names
-                    first_name, last_name = full_name.split(' ', 1) if ' ' in full_name else (full_name, '')
-
-                    if first_name:
-                        user.first_name = first_name
-                    if last_name:
-                        user.last_name = last_name
-                else:
-                    # If full name not provided, use the first part as first name
-                    user.first_name = email.split('@')[0]
+                if is_inactive:
+                    user.is_active = False
 
                 user.save()
                 if created:
                     self.stdout.write(self.style.SUCCESS(f"User '{email}' created successfully. Full name : {user.get_full_name()}"))
                 else:
-                    self.stdout.write(self.style.SUCCESS(f"Password updated for existing user '{email}'."))
+                    param_dict = {password:"Password updated", full_name: "Name updated", profile_picture: "Profile pic updated"}
+                    updated = [descr for param,descr in param_dict.items() if param]
+
+                    # list the different updated parameters when using --change_existing
+                    self.stdout.write(self.style.SUCCESS(f"{', '.join(updated)} for existing user '{email}'."))
             else:
                 self.stdout.write(self.style.WARNING(f"User '{email}' already exists. Use --change_existing to update the password."))
 
