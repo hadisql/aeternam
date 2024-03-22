40195547d1e3e0e4cccb4cd65592ea1ae8ec460a
hadi sql <hadi.sqalli@laplateforme.io>
2024-03-22 16:01:50 +0100

    fixed sessionexpiry middleware + logger info

--- core/middleware.py
+++ core/middleware.py
@@ -4,6 +4,9 @@ from datetime import datetime
 import subprocess
 from django.contrib.auth import logout
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 class SessionExpiryMiddleware:
     def __init__(self, get_response):
@@ -14,12 +17,13 @@ class SessionExpiryMiddleware:
         response = self.get_response(request)
 
         # Check if the session has expired
-        if 'test' in request.session:
+        if 'expiry_time' in request.session:
             expiry_time = request.session.get('expiry_time') # str format
-            print(f"EXPIRY TIME -> {expiry_time}")
+            logger.info(f"EXPIRY TIME -> {expiry_time}")
             expiry_time = datetime.strptime(expiry_time.replace('"',''),"%Y-%m-%d %H:%M:%S.%f") # datetime format
             current_time = datetime.now()
-            print(f"current_time.timestamp() -> {current_time.timestamp()} / expiry_time.timestamp() -> {expiry_time.timestamp()}")
+            # logger.info(f"current_time.timestamp() -> {current_time.timestamp()} / expiry_time.timestamp() -> {expiry_time.timestamp()}")
+            logger.info(f"remaining time before session expires: {int(expiry_time.timestamp()-current_time.timestamp())} seconds")
             if current_time.timestamp() > expiry_time.timestamp():
                 # Session has expired, execute the script
                 try:
