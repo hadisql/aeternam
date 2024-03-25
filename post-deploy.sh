913886a0118a12ef83836ead748f9789d37db75b
hadi sql <hadi.sqalli@laplateforme.io>
2024-03-25 15:09:12 +0100

    edited pre and post deploy scripts

--- post-deploy.sh
+++ post-deploy.sh
@@ -2,6 +2,7 @@
 
 echo "Post-deployment script.."
 
+echo "Trying to list app folder :" && ls /app && /app/staticfiles
 echo "giving execution permission to fake_data_creation file.." && chmod +x "app/staticfiles/shell_scripts/fake_data_creation.sh"
 echo "giving execution permission to fake_data_deletion file.." && chmod +x "app/staticfiles/shell_scripts/fake_data_deletion.sh"
 
--- pre-deploy.sh
+++ pre-deploy.sh
@@ -5,4 +5,22 @@ echo "Current location : " && pwd
 sleep 1
 
 echo "Trying to list mediafiles :" && ls "/app/mediafiles"
+echo "Trying to delete fake folders in mediafiles if present.."
+sleep 1
+
+# Define the directory path
+directory="/app/mediafiles"
+
+# Check if the directory exists
+if [ -d "$directory" ]; then
+    echo "Checking for folders starting with 'fake_' in $directory"
+
+    # Find folders starting with 'fake_' and delete them recursively
+    find "$directory" -type d -name 'fake_*' -exec rm -rf {} +
+
+    echo "Folders starting with 'fake_' deleted"
+else
+    echo "Error: Directory $directory not found"
+fi
+
 echo "-----------------------------------"
