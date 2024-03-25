ac5b768cdc0287744bd9058c269d776e86b0b28f
hadi sql <hadi.sqalli@laplateforme.io>
2024-03-25 14:49:03 +0100

    added sudo to postdeploy commands

--- post-deploy.sh
+++ post-deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Post-deployment script.."
 
-chmod +x /var/lib/dokku/data/storage/aeternam-dev/staticfiles/shell_scripts/fake_data_creation.sh
-chmod +x /var/lib/dokku/data/storage/aeternam-dev/staticfiles/shell_scripts/fake_data_deletion.sh
+sudo chmod +x "/var/lib/dokku/data/storage/aeternam-dev/staticfiles/shell_scripts/fake_data_creation.sh"
+sudo chmod +x "/var/lib/dokku/data/storage/aeternam-dev/staticfiles/shell_scripts/fake_data_deletion.sh"
 
-cp -rv /var/lib/dokku/data/storage/aeternam-dev/temp/fake_* /var/lib/dokku/data/storage/aeternam-dev/mediafiles
+sudo cp -rv /var/lib/dokku/data/storage/aeternam-dev/temp/fake_* /var/lib/dokku/data/storage/aeternam-dev/mediafiles
